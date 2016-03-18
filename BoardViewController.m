6cb0c12ef6b5e9cb597e8c82dbf0aff4086d05a3
ALIREZA TABRIZI <alirezatabrizi@m.local>
2016-03-18 14:27:40 -0700

    X O appears and their collors change but winner algorithm doesnt work

--- BoardViewController.m
+++ BoardViewController.m
@@ -24,7 +24,8 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 
-
+@property NSString *player1;
+@property NSString *player2;
 
 @end
 
@@ -32,27 +33,80 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // assign texts to player 1 and 2
+    self.player1 = @"Player 1 X";
+    self.player2 = @"Player 2 O";
+    
+    //set initial Player to be player 1
+    self.whichPlayerLabel.text = self.player1;
 }
 
-/*
-#pragma mark - Navigation
-
-*/
-
-
+#pragma mark - onButtonPressed
 - (IBAction)onButtonPressed:(UIButton *)sender {
-    [sender setTitle:@"X" forState:UIControlStateNormal];
+    //Check to see if label is player1
+    if ([self.whichPlayerLabel.text containsString:self.player1]) {
+        //send X to button
+        [sender setTitle:@"X" forState:UIControlStateNormal];
+        //change main label to player2
+        self.whichPlayerLabel.text = self.player2;
+        //change the color of the button to blue
+        sender.tintColor = [UIColor blueColor];
+        BOOL winner = [self horizontalWin];
+        if (winner) {
+            NSLog(@"winner");
+        }
+//        if (winner)
+//        [self whoWon:winner];
+        
+    }
+    //Check to see if label is player1
+    else if ([self.whichPlayerLabel.text containsString:self.player2]){
+        //send X to button
+        [sender setTitle:@"O" forState:UIControlStateNormal];
+        //change main label to player1
+        self.whichPlayerLabel.text = self.player1;
+        //change the color of the button to blue
+        sender.tintColor = [UIColor redColor];
+    }
 }
 
-//-(void)WhicPlayerTurn{
-//    if (/*player == 1*/) {
-//        //player = 2
-//        //update the which player label reflecting the change
-//    } else{
-//        //pleayer = 1
-//        //update the label
+//Horizontal Win
+-(BOOL)horizontalWin{
+        if (   ([self.buttonA0.titleLabel.text isEqualToString: self.buttonB0.titleLabel.text])
+            && ([self.buttonB0.titleLabel.text isEqualToString: self.buttonC0.titleLabel.text])) {
+            return YES;
+        }
+        if (   (self.buttonA1 == self.buttonB1)
+            && (self.buttonA1 == self.buttonC1)) {
+            return YES;
+        }
+        if (   (self.buttonA2 == self.buttonB2)
+            && (self.buttonA2 == self.buttonC2)) {
+            return YES;
+        }
+    return NO;
+}
+//    if (self.buttonA0 != NULL) {
+//        if (((self.buttonA0 == self.buttonB0)
+//             && (self.buttonA0 == self.buttonC0)) ||
+//            ((self.buttonA0 == self.buttonA1) && (self.buttonA0 == self.buttonA2))) {
+//            return YES;
+//        }
 //    }
-//}
+
+- (void)whoWon:(BOOL)winner{
+    if (winner) {
+        UIAlertController *whoWonAlertController = [UIAlertController alertControllerWithTitle:@"Winner" message:@"X is Winner!" preferredStyle:UIAlertControllerStyleActionSheet];
+        
+        UIAlertAction *okButton = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+            
+        }];
+        
+        [whoWonAlertController addAction:okButton];
+        
+        [self presentViewController:whoWonAlertController animated:YES completion:nil];
+    }
+}
 
 @end
